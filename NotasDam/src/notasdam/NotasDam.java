3f1a656adbcf29cc7070ec0b7ef4eea0501b313c
Curso DAM <Curso DAM@PORTATIL>
2015-10-02 16:03:18 +0200

    Proyecto Notas 1.0:
    Primera version del programa

---
+++ NotasDam/src/notasdam/NotasDam.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package notasdam;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan Jose Moya
+ */
+public class NotasDam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al evaluador de notas");
+        System.out.println("Introduce un numero");
+        Scanner num=new Scanner(System.in);
+        int nota=num.nextInt();
+        //comprobacion de la nota
+        if(nota<0||nota>10)
+            System.out.println("Datos incorrectos");
+        else{
+            if(nota<=5)
+                System.out.println("Estas suspenso con un "+nota);
+            else
+                System.out.println("Estas aprobado con un "+nota);
+        }
+    }
+    
+}
